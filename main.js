f37d4c138ebf29b32bca14127fe5ecf5549663d1
Asad Ishaq <167089251+axadishaq@users.noreply.github.com>
2025-08-22 18:13:31 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,50 @@
+const { animate, inView, stagger } = Motion;
+
+// Animate on page load
+animate(
+   ".hero-left",
+   { opacity: [0, 1], x: [-100, 0] }, // slide from left (-100px) to original
+   { duration: 1.5, easing: "ease-out" }
+);
+
+// Animate again when user scrolls back
+inView(".hero-left", ({ target, enter, leave }) => {
+   enter(() => {
+      animate(
+         target,
+         { opacity: [0, 1], x: [-100, 0] },
+         { duration: 1.5, easing: "ease-out" }
+      );
+   });
+
+   leave(() => {
+      animate(target, { opacity: 0, x: -100 }, { duration: 1 });
+   });
+});
+//child with small delay
+animate(
+   ".hero-left > *", // all direct children
+   { opacity: [0, 1], x: [-100, 0] },
+   { duration: 1, delay: stagger(0.3), easing: "ease-out" }
+);
+
+// <!-- FAQ Script  -->
+
+document.querySelectorAll(".faq-question").forEach((question) => {
+   question.addEventListener("click", function () {
+      const item = this.parentElement;
+      const isActive = item.classList.contains("active");
+      // Close all
+      document
+         .querySelectorAll(".faq-item")
+         .forEach((el) => el.classList.remove("active"));
+      // Open if not already active
+      if (!isActive) {
+         item.classList.add("active");
+      }
+   });
+});
+
 // Get all "View Project" buttons
 const projectButtons = document.querySelectorAll(".btn2");
 projectButtons.forEach((button) => {
